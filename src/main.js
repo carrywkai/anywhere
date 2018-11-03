13bb94dc50305dbd888115807af94d0c34d675cb
carry <carrywu0313@126.com>
2018-11-03 22:34:24 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 重置样式
+import 'styles/reset.css'
+// 解决移动端1px问题
+import 'styles/border.css'
+// 使用faskclick库，来解决点击300毫秒颜延迟问题，来提高用户体验
+// npm install faskclick
+import fastClick from 'fastclick'
+// 引入图标样式
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/page/home/components/HomeHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dvBox">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<!-- 使用stylus要下载二个依赖包：npm install stylus --save 和 npm install stylus-loader --save -->
+<style lang="stylus" scoped>
+  .dvBox
+    color: pink
+</style>
